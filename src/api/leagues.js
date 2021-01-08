5060eb5de01facf10a6f0e4248f760b5a902a7ef
Julia Kharlamova <juliettkhar@gmail.com>
2021-01-08 23:15:18 +0300

    add refactor api service

--- src/api/leagues.js
+++ src/api/leagues.js
@@ -1,13 +1,3 @@
-import axios from '../plugins/axios';
-
-export function getLeaguesList() {
-  return axios.get('/competitions');
-}
-
-export function getLeagueById(id, params) {
-  return axios.get(`/competitions/${id}/matches`, { params });
-}
-
 export const apiServiceLeagues = {
   getLeagueById(id, params) {
     return axios.get(`/competitions/${id}/matches`, { params });
